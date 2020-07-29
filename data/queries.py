b1825b3f9e6a779506e399b66da88d900763c141
Catalin-Daniel Despa <catalin.despa.code@gmail.com>
2020-07-29 23:38:47 +0300

    Adds all features required in the tasks

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,63 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+
+def get_all_shows():
+    return data_manager.execute_select('SELECT id, title, year, runtime, CAST(rating AS decimal(10, 1)), trailer, homepage  FROM shows ORDER BY rating DESC;')
+
+
+def get_most_rated_shows_genres():
+    return data_manager.execute_select('SELECT genres.id, genres.name, show_genres.genre_id, show_genres.show_id FROM genres JOIN show_genres ON genres.id = show_genres.genre_id ORDER BY name;')
+
+
+def get_actors():
+    return data_manager.execute_select('SELECT actors.id, actors.name, show_characters.actor_id, show_characters.show_id FROM actors JOIN show_characters ON actors.id = show_characters.actor_id ORDER BY name;')
+
+
+def get_show_seasons(show_id):
+    return data_manager.execute_select(f'SELECT * FROM seasons WHERE show_id = {show_id} ORDER BY title;')
+
+
+def shows_by_rating_desc(page):
+    offset = (page - 1) * 15
+    return data_manager.execute_select(f'SELECT id, title, year, runtime, CAST(rating AS decimal(10, 1)), trailer, homepage  FROM shows ORDER BY rating DESC LIMIT 15 OFFSET {offset};')
+
+
+def shows_by_rating_asc(page):
+    offset = (page - 1) * 15
+    return data_manager.execute_select(f'SELECT id, title, year, runtime, CAST(rating AS decimal(10, 1)), trailer, homepage  FROM shows ORDER BY rating LIMIT 15 OFFSET {offset};')
+
+
+def shows_by_year_desc(page):
+    offset = (page - 1) * 15
+    return data_manager.execute_select(f'SELECT id, title, year, runtime, CAST(rating AS decimal(10, 1)), trailer, homepage  FROM shows ORDER BY year DESC LIMIT 15 OFFSET {offset};')
+
+
+def shows_by_year_asc(page):
+    offset = (page - 1) * 15
+    return data_manager.execute_select(f'SELECT id, title, year, runtime, CAST(rating AS decimal(10, 1)), trailer, homepage  FROM shows ORDER BY year LIMIT 15 OFFSET {offset};')
+
+
+def shows_by_title_desc(page):
+    offset = (page - 1) * 15
+    return data_manager.execute_select(f'SELECT id, title, year, runtime, CAST(rating AS decimal(10, 1)), trailer, homepage  FROM shows ORDER BY title DESC LIMIT 15 OFFSET {offset};')
+
+
+def shows_by_title_asc(page):
+    offset = (page - 1) * 15
+    return data_manager.execute_select(f'SELECT id, title, year, runtime, CAST(rating AS decimal(10, 1)), trailer, homepage  FROM shows ORDER BY title LIMIT 15 OFFSET {offset};')
+
+
+def shows_by_length_desc(page):
+    offset = (page - 1) * 15
+    return data_manager.execute_select(f'SELECT id, title, year, runtime, CAST(rating AS decimal(10, 1)), trailer, homepage  FROM shows ORDER BY runtime DESC LIMIT 15 OFFSET {offset};')
+
+
+def shows_by_length_asc(page):
+    offset = (page - 1) * 15
+    return data_manager.execute_select(f'SELECT id, title, year, runtime, CAST(rating AS decimal(10, 1)), trailer, homepage  FROM shows ORDER BY runtime LIMIT 15 OFFSET {offset};')
+
+
+def get_show_by_id(id):
+    return data_manager.execute_select(f'SELECT * FROM shows WHERE id = {id};')
--- main.py
+++ main.py
@@ -1,15 +1,170 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect, jsonify, json
 from data import queries
 import math
 from dotenv import load_dotenv
 
+
 load_dotenv()
 app = Flask('codecool_series')
+
 
 @app.route('/')
 def index():
     shows = queries.get_shows()
     return render_template('index.html', shows=shows)
+
+
+@app.route('/shows/most-rated/page<int:num>')
+def shows_by_rating_desc(num):
+    shows = queries.shows_by_rating_desc(num)
+    genres = queries.get_most_rated_shows_genres()
+    all_shows = queries.get_all_shows()
+    all_pages = []
+    for i in range((len(all_shows) // 15) + 1):
+        all_pages.append(i + 1)
+        i += 1
+    shown_pages = []
+    if num in all_pages:
+        for i in range(5):
+            shown_pages.append(num + i)
+            i += 1
+    sorted = 1
+    return render_template('shows_by_rating_desc.html', shows=shows, genres=genres, pages=shown_pages, num=num, sorted=sorted)
+
+
+@app.route('/shows/lowest-rated/page<int:num>')
+def shows_by_rating_asc(num):
+    shows = queries.shows_by_rating_asc(num)
+    genres = queries.get_most_rated_shows_genres()
+    all_shows = queries.get_all_shows()
+    all_pages = []
+    for i in range((len(all_shows) // 15) + 1):
+        all_pages.append(i + 1)
+        i += 1
+    shown_pages = []
+    if num in all_pages:
+        for i in range(5):
+            shown_pages.append(num + i)
+            i += 1
+    sorted = 0
+    return render_template('shows_by_rating_asc.html', shows=shows, genres=genres, pages=shown_pages, num=num, sorted=sorted)
+
+
+@app.route('/shows/z-a/page<int:num>')
+def shows_by_title_desc(num):
+    shows = queries.shows_by_title_desc(num)
+    genres = queries.get_most_rated_shows_genres()
+    all_shows = queries.get_all_shows()
+    all_pages = []
+    for i in range((len(all_shows) // 15) + 1):
+        all_pages.append(i + 1)
+        i += 1
+    shown_pages = []
+    if num in all_pages:
+        for i in range(5):
+            shown_pages.append(num + i)
+            i += 1
+    sorted = 1
+    return render_template('shows_by_title_desc.html', shows=shows, genres=genres, pages=shown_pages, num=num, sorted=sorted)
+
+
+@app.route('/shows/a-z/page<int:num>')
+def shows_by_title_asc(num):
+    shows = queries.shows_by_title_asc(num)
+    genres = queries.get_most_rated_shows_genres()
+    all_shows = queries.get_all_shows()
+    all_pages = []
+    for i in range((len(all_shows) // 15) + 1):
+        all_pages.append(i + 1)
+        i += 1
+    shown_pages = []
+    if num in all_pages:
+        for i in range(5):
+            shown_pages.append(num + i)
+            i += 1
+    sorted = 0
+    return render_template('shows_by_title_asc.html', shows=shows, genres=genres, pages=shown_pages, num=num, sorted=sorted)
+
+
+@app.route('/shows/oldest/page<int:num>')
+def shows_by_year_desc(num):
+    shows = queries.shows_by_year_desc(num)
+    genres = queries.get_most_rated_shows_genres()
+    all_shows = queries.get_all_shows()
+    all_pages = []
+    for i in range((len(all_shows) // 15) + 1):
+        all_pages.append(i + 1)
+        i += 1
+    shown_pages = []
+    if num in all_pages:
+        for i in range(5):
+            shown_pages.append(num + i)
+            i += 1
+    sorted = 1
+    return render_template('shows_by_year_desc.html', shows=shows, genres=genres, pages=shown_pages, num=num, sorted=sorted)
+
+
+@app.route('/shows/most-recent/page<int:num>')
+def shows_by_year_asc(num):
+    shows = queries.shows_by_year_asc(num)
+    genres = queries.get_most_rated_shows_genres()
+    all_shows = queries.get_all_shows()
+    all_pages = []
+    for i in range((len(all_shows) // 15) + 1):
+        all_pages.append(i + 1)
+        i += 1
+    shown_pages = []
+    if num in all_pages:
+        for i in range(5):
+            shown_pages.append(num + i)
+            i += 1
+    sorted = 0
+    return render_template('shows_by_year_asc.html', shows=shows, genres=genres, pages=shown_pages, num=num, sorted=sorted)
+
+
+@app.route('/shows/longest-runtime/page<int:num>')
+def shows_by_length_desc(num):
+    shows = queries.shows_by_length_desc(num)
+    genres = queries.get_most_rated_shows_genres()
+    all_shows = queries.get_all_shows()
+    all_pages = []
+    for i in range((len(all_shows) // 15) + 1):
+        all_pages.append(i + 1)
+        i += 1
+    shown_pages = []
+    if num in all_pages:
+        for i in range(5):
+            shown_pages.append(num + i)
+            i += 1
+    sorted = 1
+    return render_template('shows_by_length_desc.html', shows=shows, genres=genres, pages=shown_pages, num=num, sorted=sorted)
+
+
+@app.route('/shows/shortest-runtime/page<int:num>')
+def shows_by_length_asc(num):
+    shows = queries.shows_by_length_asc(num)
+    genres = queries.get_most_rated_shows_genres()
+    all_shows = queries.get_all_shows()
+    all_pages = []
+    for i in range((len(all_shows) // 15) + 1):
+        all_pages.append(i + 1)
+        i += 1
+    shown_pages = []
+    if num in all_pages:
+        for i in range(5):
+            shown_pages.append(num + i)
+            i += 1
+    sorted = 0
+    return render_template('shows_by_length_asc.html', shows=shows, genres=genres, pages=shown_pages, num=num, sorted=sorted)
+
+
+@app.route('/show/<int:id>')
+def show_id(id):
+    show = queries.get_show_by_id(id)
+    genres = queries.get_most_rated_shows_genres()
+    actors = queries.get_actors()
+    show_seasons = queries.get_show_seasons(id)
+    return render_template('show.html', show=show, genres=genres, actors=actors, show_seasons=show_seasons)
 
 
 @app.route('/design')
